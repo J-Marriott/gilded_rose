415491d2b6675e1db82793661ba36903914fffdc
Joe Marriott <marriott90@gmail.com>
2017-03-08 15:48:55 +0000

    cleanup

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,4 +1,5 @@
 class GildedRose
+  ADJUST = 1
   MAX_QUALITY = 50
   MIN_QUALITY = 0
   SPECIAL = ['Backstage passes', 'Aged Brie', 'Conjured'].freeze
@@ -15,7 +16,7 @@ class GildedRose
       quality_change_brie(item)      if item.name == 'Aged Brie'
       quality_change_conjured(item)  if item.name[0..7] == 'Conjured'
       quality_change(item) unless SPECIAL.any? { |i| item.name.include? i }
-      max_or_min_quality(item)
+      limit_max_or_min_quality(item)
       reduce_sell_in(item)
     end
   end
@@ -23,32 +24,32 @@ class GildedRose
   private
 
   def reduce_sell_in(item)
-    item.sell_in -= 1 unless item.name[0..7] == 'Sulfuras'
+    item.sell_in -= ADJUST unless LEGENDARY.any? { |i| item.name.include? i }
   end
 
   def quality_change_brie(item)
-    item.sell_in > 0 ? item.quality += 1 : item.quality += 2
+    item.sell_in > 0 ? item.quality += ADJUST : item.quality += ADJUST * 2
   end
 
   def quality_change_backstage(item)
     amount = case
-     when item.sell_in > 10 then 1
-     when item.sell_in > 5 then 2
-     when item.sell_in > 0 then 3
-     when item.sell_in <= 0 then -item.quality
+      when item.sell_in > 10 then ADJUST
+      when item.sell_in > 5  then ADJUST * 2
+      when item.sell_in > 0  then ADJUST * 3
+      when item.sell_in <= 0 then -item.quality
     end
     item.quality += amount
   end
 
   def quality_change_conjured(item)
-    item.quality -= 2 if item.sell_in > 0
+    item.quality -= ADJUST * 2 if item.sell_in > 0
   end
 
   def quality_change(item)
-    item.sell_in > 0 ? item.quality -= 1 : item.quality -= 2
+    item.sell_in > 0 ? item.quality -= ADJUST : item.quality -= ADJUST * 2
   end
 
-  def max_or_min_quality(item)
+  def limit_max_or_min_quality(item)
     item.quality = MAX_QUALITY if item.quality > 50
     item.quality = MIN_QUALITY if item.quality < 0
   end
